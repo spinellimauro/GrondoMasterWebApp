9fba35fde5961a4272c6c909e1e3c2e5bfba1560
Gabriel Carlassare <gabrielcarlassare@gmail.com>
2020-10-05 00:07:28 -0300

    prueba

--- frontend/src/app/utils/primeng.module.ts
+++ frontend/src/app/utils/primeng.module.ts
@@ -1,26 +1,25 @@
 import { NgModule } from '@angular/core';
-import {
-  TreeTableModule,
-  SharedModule,
-  FileUploadModule,
-  DataGridModule,
-  BreadcrumbModule,
-  AutoCompleteModule,
-  InputTextModule,
-  ButtonModule,
-  MessageModule,
-  MessagesModule,
-  ToolbarModule,
-  TooltipModule,
-} from 'primeng/primeng';
+
+import { RippleModule } from 'primeng/ripple';
+import { TooltipModule } from 'primeng/tooltip';
+import { TreeTableModule } from 'primeng/treetable';
+import { FileUploadModule } from 'primeng/fileupload';
+import { TableModule } from 'primeng/table';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { ButtonModule } from 'primeng/button';
+import { MessageModule } from 'primeng/message';
+import { MessagesModule } from 'primeng/messages';
+import { ToolbarModule } from 'primeng/toolbar';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
   declarations: [],
   imports: [
     TreeTableModule,
-    SharedModule,
+    RippleModule,
     FileUploadModule,
-    DataGridModule,
+    TableModule,
     BreadcrumbModule,
     AutoCompleteModule,
     InputTextModule,
@@ -32,9 +31,9 @@ import {
   ],
   exports: [
     TreeTableModule,
-    SharedModule,
+    RippleModule,
     FileUploadModule,
-    DataGridModule,
+    TableModule,
     BreadcrumbModule,
     AutoCompleteModule,
     InputTextModule,
